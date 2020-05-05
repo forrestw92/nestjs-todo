ac585436ecb83575c594a6026c86a031c9372f6c
Forrest Walker <me@forrestwalker.net>
2020-05-05 14:44:45 -0500

    Added validation to CreateTodoInput

--- src/todo/todo.input.ts
+++ src/todo/todo.input.ts
@@ -1,12 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { TodoPriority } from './todo-priority.enum';
+import { IsArray, IsDateString, IsIn, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput {
     @Field()
+    @MinLength(1)
     name: string;
 
     @Field()
+    @IsDateString()
     scheduledDate: string;
 
     @Field(() => [String], { defaultValue: [], nullable: true })
