fb8f879974e341620290522ca6a494be68341ed0
Forrest Walker <me@forrestwalker.net>
2020-05-04 22:18:44 -0500

    Init typeorm and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/todo.entity';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [TodoModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            url: 'mongodb://localhost/todo',
+            useUnifiedTopology: true,
+            synchronize: true,
+            entities: [Todo],
+        }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+        }),
+        TodoModule,
+    ],
 })
 export class AppModule {}
