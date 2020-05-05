87fc3f1baa664f006bcae998f283faa4853bb10b
Forrest Walker <me@forrestwalker.net>
2020-05-05 14:02:28 -0500

    Added createTodo mutation and todos query

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { TodoService } from './todo.service';
+import { TodoType } from './todo.type';
+import { TodoPriority } from './todo-priority.enum';
+import { CreateTodoInput } from './todo.input';
 
 @Resolver('Todo')
-export class TodoResolver {}
+export class TodoResolver {
+    constructor(private todoService: TodoService) {}
+
+    @Mutation(returns => TodoType)
+    createTodo(@Args('todoInput') todoInput: CreateTodoInput) {
+        return this.todoService.createTodo(todoInput);
+    }
+
+    @Query(returns => TodoType)
+    todos() {
+        return this.todoService.getAllTodos();
+    }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
+import { v4 as uuid } from 'uuid';
+import { TodoPriority } from './todo-priority.enum';
+import { CreateTodoInput } from './todo.input';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    constructor(
+        @InjectRepository(Todo)
+        private readonly todoRepository: Repository<Todo>,
+    ) {}
+    async createTodo(todoInput: CreateTodoInput): Promise<Todo> {
+        const { priority, labels, name, scheduledDate } = todoInput;
+        const todo = await this.todoRepository.create({
+            id: uuid(),
+            name,
+            scheduledDate,
+            labels: labels.length > 0 ? labels : [],
+            priority: priority !== null ? priority : TodoPriority.P4,
+        });
+        return this.todoRepository.save(todo);
+    }
+    async getAllTodos(): Promise<Todo[]> {
+        return this.todoRepository.find();
+    }
+}
