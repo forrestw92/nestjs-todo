c6cc680141fb2c86e94426186902f5f9c1ee9a96
Forrest Walker <me@forrestwalker.net>
2020-05-04 22:11:58 -0500

    Added todo type

---
+++ src/todo/todo.type.ts
@@ -0,0 +1,20 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { TodoPriority } from './todo-priority.enum';
+
+@ObjectType('Todo')
+export class TodoType {
+    @Field()
+    id: string;
+
+    @Field()
+    name: string;
+
+    @Field()
+    scheduledDate: string;
+
+    @Field()
+    labels: string[];
+
+    @Field(type => TodoPriority)
+    priority: TodoPriority;
+}
