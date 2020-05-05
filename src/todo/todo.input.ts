083e862f43988f168fb059d508e5fedf4e43173d
Forrest Walker <me@forrestwalker.net>
2020-05-05 13:58:31 -0500

    Let labels and priority fields be null

---
+++ src/todo/todo.input.ts
@@ -0,0 +1,20 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { TodoPriority } from './todo-priority.enum';
+
+@InputType()
+export class TodoInput {
+    @Field()
+    id: string;
+
+    @Field()
+    name: string;
+
+    @Field()
+    scheduledDate: string;
+
+    @Field(() => [String], { defaultValue: [], nullable: true })
+    labels: string[];
+
+    @Field(type => TodoPriority, { nullable: true })
+    priority: TodoPriority;
+}
