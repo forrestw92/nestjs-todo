cc88a9585913982ab76bc3ed2d5495304c8a68f3
Forrest Walker <me@forrestwalker.net>
2020-05-05 13:53:58 -0500

    Registered TodoPriority as type

--- src/todo/todo-priority.enum.ts
+++ src/todo/todo-priority.enum.ts
@@ -1,6 +1,11 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum TodoPriority {
-    P1,
+    P1 = 1,
     P2,
     P3,
     P4,
 }
+registerEnumType(TodoPriority, {
+    name: 'TodoPriority',
+});
